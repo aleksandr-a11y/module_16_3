ec1cce5788b5c61d062b580c15e12cdc00f8074a
aleksandr-a11y <117071816+aleksandr-a11y@users.noreply.github.com>
2024-12-07 14:35:43 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -17,8 +17,9 @@ async def post_users(username: Annotated[str, Path(min_length=3, max_length=20, 
     return f"User {user_id} is registered"
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def put_users(user_id: str, username: Annotated[str, Path(min_length=3, max_length=20, regex="^[a-zA-Z0-9_-]+$")],
-                    age: Annotated[int, Path(gt=18, le=100, description="Age from 18 to 100")]) -> str:
+async def put_users(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='75')],
+                    username: Annotated[str, Path(min_length=3, max_length=20, regex="^[a-zA-Z0-9_-]+$")],
+                    age: Annotated[int, Path(ge=18, le=100, description="Age from 18 to 100")]) -> str:
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is updated"
 
